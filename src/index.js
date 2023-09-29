7e73fb08277ffc9d5ac038841fda7ee8e180d8c0
shafiaansar <shafiaansar142@gmail.com>
2023-09-29 12:06:56 +0500

    testing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import 'react-toastify/dist/ReactToastify.css'; // Import the styles
 import { ToastContainer } from 'react-toastify';
 import { BrowserRouter } from 'react-router-dom';
-
+// Testing
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
